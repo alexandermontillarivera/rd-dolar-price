a93d4478d4b699b467a9bb4c30841637ce4ef173
Alexander Montilla Rivera <alexander@codika.net>
2024-04-10 14:15:46 -0400

    Add cors package and update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,16 @@
 import "https://deno.land/x/puppeteer@16.2.0/install.ts"
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
 import * as dotenv from "https://deno.land/std@0.221.0/dotenv/mod.ts"
-export * as path from "https://deno.land/std@0.184.0/path/mod.ts"
+import * as path from "https://deno.land/std@0.184.0/path/mod.ts"
+// @deno-types="npm:@types/cors"
+import cors from 'npm:cors'
 // @deno-types="npm:@types/express"
 import express from "npm:express"
 
-export { dotenv, express, puppeteer }
+export { 
+  dotenv, 
+  express, 
+  puppeteer, 
+  cors, 
+  path
+}
