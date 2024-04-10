e6d4af93d8a8d30e7581558943ad446d6b302630
Alexander Montilla Rivera <alexander@codika.net>
2024-04-10 10:24:19 -0400

    Add welcome message to root endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,14 @@ import { app } from "@/server.ts"
 import { enviroments } from "@config/enviroments.ts"
 import pricesRoutes from "@routes/prices.ts"
 
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Welcome to the API"
+  })
+})
+
 app.use("/api/prices", pricesRoutes)
+
 
 app.listen(enviroments.PORT, () => {
   console.info(`[SERVER]: Running in http://localhost:${enviroments.PORT}`)
