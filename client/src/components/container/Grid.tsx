92f149010a6ff6560fc1a3f1a9c06dd07a86ae6d
Alexander Montilla Rivera <alexander@codika.net>
2024-04-11 15:23:20 -0400

    Add mobile grid properties to Grid component

--- client/src/components/container/Grid.tsx
+++ client/src/components/container/Grid.tsx
@@ -1,3 +1,4 @@
+import styles from '@styles/components/container/Grid.module.css'
 interface Props {
   children: React.ReactNode
   maxWidth: string
@@ -7,20 +8,43 @@ interface Props {
   alignItems: string
   gap: string
   margin: string
+  mobileMinGridWidth: string
+  mobileMaxGridWidth: string
+  mobileGap: string
 }
 
-export function Grid ({ children, maxGridWidth = '1fr', minGridWidth = '1fr', gap = '1rem', justifyContent = 'space-between', maxWidth = '1200px', margin = '0px', alignItems = 'inherit' }: Partial<Props>) {
+export function Grid (props: Partial<Props>) {
+  const {
+    children,
+    maxGridWidth = '1fr',
+    minGridWidth = '1fr',
+    gap = '1rem',
+    justifyContent = 'space-between',
+    maxWidth = '1200px',
+    margin = '0px',
+    alignItems = 'inherit',
+    mobileGap = '1rem',
+    mobileMaxGridWidth = '1fr',
+    mobileMinGridWidth = '1fr'
+  } = props
+
   return (
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: `repeat(auto-fill, minmax(${minGridWidth}, ${maxGridWidth}))`,
-      gap,
-      width: '100%',
-      justifyContent,
-      maxWidth,
-      margin,
-      alignItems
-    }}>
+    <div
+      className={styles.container}
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(auto-fill, minmax(${minGridWidth}, ${maxGridWidth}))`,
+        gap,
+        width: '100%',
+        justifyContent,
+        maxWidth,
+        margin,
+        alignItems
+      }}
+      data-min-mobile-width={mobileMinGridWidth}
+      data-max-mobile-width={mobileMaxGridWidth}
+      data-mobile-gap={mobileGap}
+      >
       {children}
     </div>
   )
