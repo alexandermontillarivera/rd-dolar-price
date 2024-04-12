d95656915fb89b49de25afd75fc5a9c5e403332f
Alexander Montilla Rivera <alexander@codika.net>
2024-04-12 09:48:18 -0400

    Update static file serving path

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import { express, cors, serveStatic } from "@dependencies"
-import { STATIC_FILES_PATH } from "@constants"
-
+import { express, cors, serveStatic, path } from "@dependencies"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-app.use(serveStatic(STATIC_FILES_PATH))
+app.use(serveStatic(path.join(Deno.cwd(), "public")))
 
 export { app }
